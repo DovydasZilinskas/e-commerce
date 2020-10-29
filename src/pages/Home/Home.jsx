2a2b86a9c6f67a67436d5931c120155e44cdbb80
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-10-29 18:30:15 +0200

    Updated Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,54 @@
-import React from "react";
-import { Button, Section } from "../../components";
+import React, { useState, useEffect, useContext } from "react";
+import { InputField, Section, Button } from "../../components";
+import * as S from "./Home.style";
+import { useHistory } from "react-router-dom";
+import { LocationContext } from "../../context/location.context";
 
 function Home() {
+  const location = useContext(LocationContext);
+  const [postCode, setPostCode] = useState();
+  const [error, setError] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => setPostCode(location.state), [location.state]);
+
   return (
     <>
-      <Section background="#eee" maxWidth="max">
-        Welcome to my boilerplate
-      </Section>
       <Section>
-        <Button color="primary" handeClick={() => alert("Labas bl")}>
-          Click me
-        </Button>
-        <Button color="secondary" handeClick={() => alert("Viso gero nx")}>
-          Don't Click me
-        </Button>
-      </Section>
-      <Section background="#bbb">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae,
-        dolorem. Ipsam quaerat itaque quasi sequi necessitatibus, labore eius
-        vitae ad quo iure odit porro non aliquid reprehenderit magnam et
-        distinctio?
+        <h1>Select Your Store</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (postCode) {
+              setError(false);
+              location.setState(postCode);
+              history.push("/about");
+            } else {
+              setError(true);
+            }
+          }}
+        >
+          {error && (
+            <S.Error>
+              Post Code is incorrect. Please check and try again.
+            </S.Error>
+          )}
+
+          <InputField
+            type="dropdown"
+            arr={[
+              { city: "Kaunas" },
+              { city: "Vilnius" },
+              { city: "Siauliai" },
+            ]}
+            handleChange={(e) => setPostCode(e.target.value)}
+          />
+          <S.InputBlock>
+            <Button type="submit" color="primary">
+              Next
+            </Button>
+          </S.InputBlock>
+        </form>
       </Section>
     </>
   );
---
+++ src/pages/Home/Home.style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const InputBlock = styled.div`
+  text-align: right;
+  margin-top: 1em;
+`;
+
+export const Error = styled.div`
+  background-color: orange;
+  font-size: 1.4em;
+  padding: 10px;
+  border-radius: 10px;
+  text-align: center;
+  border: 1px solid gray;
+  margin-bottom: 1em;
+`;
