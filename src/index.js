87ab8d730e5de069d2eb8921f495d81ea2efeec1
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-10-29 18:30:26 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import { ThemeProvider } from "styled-components";
+import { LocationProvider } from "./context/location.context";
 import theme from "./theme";
 import "normalize.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Routes />
+      <LocationProvider>
+        <Routes />
+      </LocationProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
