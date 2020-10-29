1fed53d737ea7924d9013fe6315e7e8d8878cf94
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-10-29 18:30:07 +0200

    Added product list table

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,63 @@
-import React from "react";
-import { Section } from "../../components";
+import React, { useContext, useEffect, useState } from "react";
+import { Section, Button } from "../../components";
+import { LocationContext } from "../../context/location.context";
+import { useHistory, Link } from "react-router-dom";
+import * as S from "./About.style";
 
 function About() {
-  return <Section>About</Section>;
+  const location = useContext(LocationContext);
+  const history = useHistory();
+  const [products, setProducts] = useState();
+
+  if (!location.state) {
+    history.push("/");
+  }
+
+  useEffect(() => {
+    if (location.state) {
+      fetch(`http://127.0.0.1:8080/${location.state}`)
+        .then((res) => res.json())
+        .then((data) => setProducts(data));
+    }
+  }, [location.state]);
+
+  return (
+    <>
+      <Section>
+        <S.FlexBox>
+          <h1>Product List</h1>
+          <Link to="/">
+            <Button>Go Back</Button>
+          </Link>
+        </S.FlexBox>
+      </Section>
+      <Section>
+        <h2>
+          {location.state &&
+            location.state.charAt(0).toUpperCase() + location.state.slice(1)}
+        </h2>
+        <S.Table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products &&
+              products.map((product) => (
+                <tr key={product.id}>
+                  <td>{product.title}</td>
+                  <td>{product.description}</td>
+                  <td>{product.price / 100} Eur</td>
+                </tr>
+              ))}
+          </tbody>
+        </S.Table>
+      </Section>
+    </>
+  );
 }
 
 export default About;
---
+++ src/pages/About/About.style.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const FlexBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  border: 1px solid black;
+  border-collapse: collapse;
+  & th {
+    border-bottom: 2px solid #444;
+    background-color: ${(props) => props.theme.secondary.background};
+    padding: 5px;
+  }
+  & td {
+    border: 1px dotted black;
+    padding: 5px;
+  }
+`;
+
+export const Error = styled.div`
+  background-color: orange;
+  font-size: 1.4em;
+  padding: 10px;
+  border-radius: 10px;
+  text-align: center;
+  border: 1px solid gray;
+  margin-bottom: 1em;
+`;
