d95b2112b704dcfe513aab3341757325e5bb4c79
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-10-30 23:22:39 +0200

    fixed history and data validation issues, added component

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Section, Button, Loading } from "../../components";
+import { Section, Button, Loading, Table } from "../../components";
 import { UserContext } from "../../context/user.context";
 import { useHistory, Link } from "react-router-dom";
 import * as S from "./Products.style";
@@ -9,56 +9,42 @@ function About() {
   const history = useHistory();
   const [products, setProducts] = useState();
 
-  if (!user.state) {
-    history.push("/");
-  }
-
   useEffect(() => {
     if (user.state && user.state.city) {
       fetch(`http://127.0.0.1:8080/${user.state.city}`)
         .then((res) => res.json())
         .then((data) => setProducts(data));
+    } else {
+      history.push("/");
     }
-  }, [user.state]);
+  }, [user.state, history]);
 
   return (
     <>
       <Section>
         <S.FlexBox>
-          <h1>
-            Hello {user.state && user.state.name}, this is your product list.
-          </h1>
+          <S.HeadingText>
+            Hello{user.state.gender && " "}
+            {user.state && user.state.gender}
+            {user.state.name && " "}
+            {user.state && user.state.name}
+            {user.state && ", "}
+            {!products || products.length === 0
+              ? "your products has not been found."
+              : "this is your product list."}
+          </S.HeadingText>
           <Link to="/">
             <Button>Go Back</Button>
           </Link>
         </S.FlexBox>
       </Section>
       <Section>
-        <S.Table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products &&
-              products.map((product) => (
-                <tr key={product.id}>
-                  <td>{product.title}</td>
-                  <td>{product.description}</td>
-                  <td>{product.price / 100} Eur</td>
-                </tr>
-              ))}
-          </tbody>
-        </S.Table>
         {!products ? (
           <Loading />
         ) : !products || products.length === 0 ? (
           <S.Error>Data not found</S.Error>
         ) : (
-          ""
+          <Table products={products} />
         )}
       </Section>
     </>
--- src/pages/Products/Products.style.js
+++ src/pages/Products/Products.style.js
@@ -6,28 +6,17 @@ export const FlexBox = styled.div`
   align-items: center;
 `;
 
-export const Table = styled.table`
-  width: 100%;
-  border: 1px solid black;
-  border-collapse: collapse;
-  & th {
-    border-bottom: 2px solid #444;
-    background-color: ${(props) => props.theme.secondary.background};
-    padding: 5px;
-  }
-  & td {
-    border: 1px dotted black;
-    padding: 5px;
-  }
-`;
-
 export const Error = styled.div`
   margin-top: 10px;
-  background-color: orange;
+  background-color: ${(props) => props.theme.global.background};
   font-size: 1.4em;
   padding: 10px;
   border-radius: 10px;
   text-align: center;
   border: 1px solid gray;
   margin-bottom: 1em;
+`;
+
+export const HeadingText = styled.h1`
+  font-size: 1.4em;
 `;
