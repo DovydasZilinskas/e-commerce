bb7aed45c889aabe73579b1e557e48be15e16672
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-10-30 23:17:46 +0200

    removed button margin

--- src/components/Button/Button.style.js
+++ src/components/Button/Button.style.js
@@ -8,7 +8,6 @@ export const Button = styled.button`
   display: inline-block;
   padding: 0.5em 3em;
   border: 0.16em solid #ffffff;
-  margin: 0 0.3em 0.3em 0;
   box-sizing: border-box;
   text-decoration: none;
   text-transform: uppercase;
